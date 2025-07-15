f407b9c07c3ec4ae605cbbdb87d213aaf7ff288e
Vanderson Nogueira <vanderson.santos@prill.com.br>
2025-07-15 17:59:28 -0300

    first commit

---
+++ src/main/java/br/com/home/ia/voice_transcription/VoiceTranscriptionApplication.java
@@ -0,0 +1,13 @@
+package br.com.home.ia.voice_transcription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceTranscriptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoiceTranscriptionApplication.class, args);
+	}
+
+}
